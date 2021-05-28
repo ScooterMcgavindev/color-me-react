1544af092b1a2d91eaf9b0e6278abed054dd1424
Scooter <scott.skrobel@gmail.com>
2021-05-28 15:33:44 -0600

    added Palette Footer and integrated react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 //import "../styles/index.css";
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedPalettes from './seedPalettes';
 import { genPalette } from './colorHelpers';
 
 class App extends Component {
   render() {
-    console.log(genPalette(seedPalettes[4]))
     return (
-      <div>
-        <Palette palette={genPalette(seedPalettes[4])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette List Here</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>Single Palette</h1>} />
+      </Switch>
+      //      <div>
+      //        <Palette palette={genPalette(seedPalettes[4])} />
+      //      </div>
     );
   }
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -21,10 +21,10 @@ class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
     return (
       <div className='Palette'>
@@ -35,8 +35,10 @@ class Palette extends Component {
         />
         {/* Navbar goes here */}
         <div className='Palette-colors'>{colorBoxes}</div>
-        {/* multiple color boxes */}
-        {/* footer */}
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
