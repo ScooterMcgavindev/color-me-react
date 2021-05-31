16461162f4ca838766d25b44de80d512bcc4b14f
Scooter <scott.skrobel@gmail.com>
2021-05-31 15:32:43 -0600

    add Drawer to paletteForm. updated webpack pkg

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,12 @@ module.exports = {
   presets: ["@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }], // dont need to import react when just using jsx in a file
   ],
+  "plugins": [
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        "loose": true
+      }
+    ]
+  ]
 };
