2d394d13b88c588286594adeefa36b636b3b4fa9
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-08-24 22:30:08 -0600

    saved palettes to local storage

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,8 @@ import { genPalette } from './colorHelpers';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedPalettes };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedPalettes };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -21,7 +22,17 @@ class App extends Component {
     });
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] })
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    // save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     return (
