554967092ec991645cf3d44c30713437f085ab8f
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-08-31 00:30:11 -0600

    added confirmation delete dialog

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -12,7 +12,7 @@ class MiniPalette extends Component {
   // define delete palette which accepts an event
   deletePalette(e){
     e.stopPropagation();
-    this.props.handleDelete(this.props.id);
+    this.props.openDialog(this.props.id);
   }
   render(){
     const { classes, paletteName, emoji, colors, handleClick } = this.props;
