b1b447ae3fa02dcf1890debe7fff5bd10acb4744
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-08-31 23:12:53 -0600

    fix bug dragging colorbox

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -153,6 +153,7 @@ class NewPaletteForm extends Component {
             removeColor={this.removeColor}
             axis='xy'
             onSortEnd={this.onSortEnd}
+            distance={20}
           />
         </main>
       </div>
